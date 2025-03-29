189ab4ec8620625ffe5d1095e999c2e02405f256
Sveta <svetikred77@gmail.com>
2025-03-29 20:21:02 +0200

    fix swiper

--- src/index.js
+++ src/index.js
@@ -10,27 +10,71 @@ openBtn.addEventListener('click', () => {
 // ---------- swiper ----------
 const swiper1 = new Swiper('.swiper1', {
   loop: true,
+  speed: 1500,
 
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+    type: 'bullets',
   },
 });
 
 const swiper2 = new Swiper('.swiper2', {
-  // loop: true,
+  loop: true,
+
+  speed: 1500,
+  sccMode: false,
+
+  effect: 'slide',
+  spaceBetween: 20,
+  slidesPerView: 1,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-arrow__right',
+    prevEl: '.swiper-arrow__left',
+  },
+
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
   },
 });
 
 const swiper3 = new Swiper('.swiper3', {
   loop: true,
+  speed: 1500,
+
+  effect: 'slide',
+  spaceBetween: 20,
+  slidesPerView: 1,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-arrow__right',
+    prevEl: '.swiper-arrow__left',
+  },
+
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
   },
 });
