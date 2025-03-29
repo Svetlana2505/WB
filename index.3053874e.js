90542e79c694603f2c4f32a3178c2b675bbf7ed0
Svetlana2505 <svetikred77@gmail.com>
2025-03-29 18:21:29 +0000

    Deploying to gh-pages from @ Svetlana2505/WB@189ab4ec8620625ffe5d1095e999c2e02405f256 🚀

--- index.3053874e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".header-button"),n=document.querySelector(".nav-menu");e.addEventListener("click",(()=>{n.classList.toggle("menu--active")}));new Swiper(".swiper1",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0}}),new Swiper(".swiper2",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".swiper3",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.3053874e.js.map
--- index.98eeaa98.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".header-button"),n=document.querySelector(".nav-menu");e.addEventListener("click",(function(){n.classList.toggle("menu--active")}));new Swiper(".swiper1",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0}}),new Swiper(".swiper2",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".swiper3",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})}();
-//# sourceMappingURL=index.98eeaa98.js.map
